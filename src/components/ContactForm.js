b698512dba3d0c71f84190048baba2f25ccfb847
Clent Vincent Bacatan <bacatanclentvincent@gmail.com>
2024-02-24 12:28:25 +0800

    added typescript

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,106 +8,72 @@ const formURL = 'https://getform.io/f/53a8a898-f938-4a29-ab3e-29cb2b8da59a'
 
 const ContactForm = () => {
 
+const [email, setEmail] = useState('')
+const [active, setActive] = useState(true)
 
-    const [email, setEmail] = useState('')
-    const [active, setActive] = useState(true)
+const pattern = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
 
-    const pattern = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
-    
-    
-    function handleEmail(e) {
-        
-        const buttons =  document.getElementById('submit-button');
-
+   function handleEmail(e) {
+      const buttons = document.getElementById('submit-button');
         const newEmail = e.target.value
-        setEmail(newEmail);
-        console.log(newEmail);
-    
-        if(pattern.test(newEmail)) {
+      setEmail(newEmail);
+      
+        if (pattern.test(newEmail)) {
             setActive(false);
             buttons.removeAttribute("type")
-    
         } else if (newEmail === '') {
             setActive(true);
             buttons.setAttribute("type", "button")
-         
         } else {
             setActive(true);
         }
-
         return newEmail;
-
     }
-    
+
     function handleButton() {
-        if(email === '') {
-            swal({
-                title: 'Oops!',
-                text: 'Please enter your email address',
-                icon: 'error',
-            });
-        } else if (pattern.test(email)) {
-            swal({
-                title: 'Yay!!',
-                text: 'Thank you for your message',
-                icon: 'success',
-            });
-        
+      if (email === '') {
+        swal({
+            title: 'Oops!',
+            text: 'Please enter your email address',
+            icon: 'error',
+        });
+      } else if (pattern.test(email)) {
+         swal({
+             title: 'Yay!!',
+             text: 'Thank you for your message',
+             icon: 'success',
+         });
         } else {
-            swal({
-                title: 'Oops!',
-                text: 'Please enter a valid email address',
-                icon: 'error',
-            });
+         swal({
+             title: 'Oops!',
+             text: 'Please enter a valid email address',
+             icon: 'error',
+         });
         }
-               
-        
     }
-
-    // useEffect(() => {
-
-    //     pattern.test(email)
-
-    //     if (pattern.test(email)) {
-    //         console.log(email)
-    //         setActive(a => false);
-    //         removeAttribute()
- 
-    //     } else if(email === '') {
-    //         setActive(a => true);
-
-    //     } else {
-    //         setActive(a => true);
-                   
-        
-    //     }
-        
-    // },[email]);
-  
-
-    return ( 
+    return (
 
         <section>
             <div className='w-full h-screen lg:max-h-min flex justify-center items-center p-5'>
                 <form method='POST' action={formURL} className='flex flex-col max-w-[600px] w-full drop-shadow-md '>
                     <div className='text-center py-3 drop-shadow-xl'>
                         <p className="lg:text-3xl text-lg font-medium">Contact Form</p>
-                    </div>
-                    <input className='m-2 p-3' type='text' placeholder='Enter Name' name='name'/>
-                    <input className='m-2 p-3' 
-                    type='email' 
-                    placeholder='Enter Email' 
-                    name='email'
-                    value={email}
-                    onChange={handleEmail}
+                     </div>
+                    <input className='m-2 p-3' type='text' placeholder='Enter Name' name='name' />
+                    <input className='m-2 p-3'
+                        type='email'
+                        placeholder='Enter Email'
+                        name='email'
+                        value={email}
+                        onChange={handleEmail}
                     />
-                    <textarea className="m-2 p-3" placeholder='Your Message Here' name='message' cols='30' rows='10'/>
+                    <textarea className="m-2 p-3" placeholder='Your Message Here' name='message' cols='30' rows='10' />
 
-                     <button id="submit-button" type="button"
-                     onClick={handleButton}
-              
-                     className={active ? 'submit-button border-2 rounded-full border-solid border-cyan-300 py-2 mt-4 font-semibold text-xl enabled:opacity-25' : 'submit-button border-2 rounded-full border-solid border-cyan-300 py-2 mt-4 font-semibold text-xl disabled:opacity-75'}
-                     >Send</button>
+                    <button id="submit-button" type="button"
+                        onClick={handleButton}
+
+                        className={active ? 'submit-button border-2 rounded-full border-solid border-cyan-300 py-2 mt-4 font-semibold text-xl enabled:opacity-25' : 'submit-button border-2 rounded-full border-solid border-cyan-300 py-2 mt-4 font-semibold text-xl disabled:opacity-75'}
+                    >Send</button>
                 </form>
             </div>
         </section>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,67 +1,99 @@
-import React, {useState, useEffect} from "react";
-import { AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
-import  SocialIcons from '../components/SocialIcons';
+import React, { useState, useEffect } from "react";
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import SocialIcons from './SocialIcons';
+
+
+// REVAMPING:
+// * Must be responsive in any screens
+// * The transition will cover the whole screen. Motion is from LEFT TO RIGHT
+//  reference: https://evervault.com/?ref=godly
+// * Fonts must be big
 
 
 
-    const Navbar = () => {
-         const [nav, setNav] = useState(true);
-        
-         // This function will set the menuOutline to either AiOutlineClose or AiOutlineMenu when clicked
-            const handleNav = () => {
-                setNav(!nav);
-        
-                }
-    
+const Navbar = () => {
+    const [nav, setNav] = useState(true);
+    // This function will set the menuOutline to either AiOutlineClose or AiOutlineMenu when clicked
+    const handleNav = () => {
+     setNav(!nav);
+    }
 
-        // Handles the Scroll features
-    
-        useEffect(() => {
-            const nav= document.querySelector('nav');
-                window.onscroll = function () {
-                    
-            if(document.documentElement.scrollTop > 10) {
+
+    // Handles the Scroll features
+
+    useEffect(() => {
+        const nav = document.querySelector('nav');
+        window.onscroll = function () {
+            if (document.documentElement.scrollTop > 10) {
                 nav.classList.add("bg-slate-400", "top-0", "z-10");
             } else {
                 nav.classList.remove("bg-slate-400", "top-0");
             }
         }
     })
-
-    return (    
-        
-    <nav className="flex sticky top-0 items-center md:flex-row justify-between h-20 lg:min-w-full mx-auto px-4 ">
-            <h1 className="w-full md:w-8 lg:ml-28 md:ml-7 text-3xl font-medium"><a href="/">CB</a></h1>
-        <ul className="hidden md:flex md:flex-row">
-            <li className="p-5 font-serif font-black text-xl"><a href="/about">About</a></li>
-            <li className="p-5 font-serif font-black text-xl"><a href="/project">Projects</a></li>
-            <li className="p-5 font-serif font-black text-xl" ><a href="/skills">Tools</a></li>
-            <li className="p-5 font-serif font-black text-xl"><a href="/contactform">Contact</a></li>
-        </ul>
-
-        {/* If clicked the 'AioutlineClose' will render */}
-        {/*  The "block md:hidden" will hide the outLine on medium size to large size */}
-        <div onClick={handleNav} className="block md:hidden">
-            {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}   
-        </div>
-
-        <div className={!nav ? "fixed left-0 top-0 w-{60%} border-r h-full mr-3 transition ease-in-out duration-500 bg-blue-50" :"fixed h-full -left-20 top-0.5 transition ease-in-out duration-500"}>
-            <h1 className="w-full text-3xl ml-5 mt-5 font-medium">CB</h1>
-            <ul className="pt-4">
-                <li onClick={handleNav} className="p-5 font-serif"><a href="/about">About</a></li>
-                <li onClick={handleNav} className="p-5 font-serif" ><a href="/project">Projects</a></li>
-                <li onClick={handleNav} className="p-5 font-serif" ><a href="/skills">Tools</a></li>
-                <li onClick={handleNav} className="p-5 font-serif"><a href="/contactform">Contact</a></li>
-            </ul>
-
-        {/* Social Media Handles */}
-            <div className='sm:flex mr-1'>
-                <SocialIcons/>
-            </div>
-        </div>
-    </nav>
-
-     );
-}
  
+    return (
+     <nav class={undefined}>
+      <div class={style.logoBurgerContainer}>
+       
+       <div class={style.logoContainer}>
+           <h1 class={style.logo}><a href="/">CB</a></h1>
+       </div>
+
+       <div class={style.burger}>
+         <button onClick={handleNav}>
+           {nav ? <AiOutlineMenu size={24}/> : <AiOutlineClose size={24}/>}
+          </button> 
+       </div>
+      </div>
+          
+      <div>
+        <ul class="hidden md:flex md:flex-row">
+          <li class="p-5 font-serif font-black text-xl"><a href="/about">About</a></li>
+          <li class="p-5 font-serif font-black text-xl"><a href="/project">Projects</a></li>
+          <li class="p-5 font-serif font-black text-xl" ><a href="/skills">Tools</a></li>
+          <li class="p-5 font-serif font-black text-xl"><a href="/contactform">Contact</a></li>
+        </ul>
+      </div>
+
+      {/* SideNav */}
+           
+      <div class={!nav ? style.sideNav : style.sideNavHidden}>
+        <div>
+         <div class="w-full text-3xl ml-5 mt-5 font-medium">CB</div>
+        </div>
+        
+        <div id="sideNav" class={style.sideNavListContainer}>
+          <ul class={style.sideNavLists}>
+              <li onClick={handleNav} class=""><a href="/about">About</a></li>
+              <li onClick={handleNav} class="" ><a href="/project">Projects</a></li>
+              <li onClick={handleNav} class="" ><a href="/skills">Tools</a></li>
+              <li onClick={handleNav} class=""><a href="/contactform">Contact</a></li>
+           </ul>
+        </div>
+        
+        <div class='sm:flex mr-1'>
+            <SocialIcons />
+        </div>
+      </div>
+        </nav>
+
+    );
+}
+
 export default Navbar;
+
+const style = {
+  mainContainer: "flex sticky top-0 items-center md:flex-row justify-between h-20 lg:min-w-full mx-auto px-4",
+  logoBurgerContainer: "flex w-screen justify-between mt-7",
+  logoContainer: "ml-4",
+  logo: "text-3xl font-medium",
+  burger: "mr-4 mt-1.5 md:hidden",
+  sideNav: "fixed left-0 top-0 w-9/12 border-r h-full mr-3 transition ease-in-out duration-500 bg-blue-100 font-poppins",
+  sideNavHidden: "fixed h-full -left-2/4 top-0.5 transition ease-in-out duration-500 ",
+  sideNavLists: "font-serif font-black ml-4 h-80 flex flex-col justify-around text-xl bg-red-100 md:flex md:flex-row"
+
+
+
+}
+  
